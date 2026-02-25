999ae851ecda3e75adc7788196e0824e5edfec44
yedidworembai <paitworem@gmail.com>
2026-02-26 00:56:18 +0700

    Modifikasi Jobsheet2

--- Jobsheet2/Mahasiswa28.java
+++ Jobsheet2/Mahasiswa28.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class Mahasiswa28 {
 
     
@@ -42,49 +42,5 @@ void updateIPK(double ipkBaru) {
             return "Kinerja Kurang";
         }
     }
-=======
-public class Mahasiswa28 {
 
-    
-    String nama;
-    String nim;
-    String kelas;
-    double ipk;
-
-    
-    void tampilkanInformasi() {
-        System.out.println("Nama  : " + nama);
-        System.out.println("NIM   : " + nim);
-        System.out.println("Kelas : " + kelas);
-        System.out.println("IPK   : " + ipk);
-    }
-
-   
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIPK(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }
-    }
-
-    String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja Sangat Baik";
-        } 
-        else if (ipk >= 3.0) {
-            return "Kinerja Baik";
-        }
-        else if (ipk >= 2.0) {
-            return "Kinerja Cukup";
-        }
-        else {
-            return "Kinerja Kurang";
-        }
-    }
->>>>>>> c436fdae7691ece20bcb0c2c76f3002b23586351
 }
--- Jobsheet2/MahasiswaDemo28.java
+++ Jobsheet2/MahasiswaDemo28.java
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
+
 public class MahasiswaDemo28 {
-   
+
     public static void main(String[] args) {
         Mahasiswa28 mhs = new Mahasiswa28();
         mhs.nama = "Bismillah";
         mhs.nim = "123456";
-        mhs.kelas = "TI-1A";
+        mhs.kelas = "TI-1A";        
         mhs.updateIPK(3.6);
         mhs.tampilkanInformasi();
         System.out.println("Nilai Kinerja : " + mhs.nilaiKinerja());
     }
 }
-
-=======
-public class MahasiswaDemo28 {
-   
-    public static void main(String[] args) {
-        Mahasiswa28 mhs = new Mahasiswa28();
-        mhs.nama = "Bismillah";
-        mhs.nim = "123456";
-        mhs.kelas = "TI-1A";
-        mhs.updateIPK(3.6);
-        mhs.tampilkanInformasi();
-        System.out.println("Nilai Kinerja : " + mhs.nilaiKinerja());
-    }
-}
-
->>>>>>> c436fdae7691ece20bcb0c2c76f3002b23586351
