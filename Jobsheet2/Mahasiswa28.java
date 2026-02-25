2508c20451af1d6090f7f8cb6ec71a687e0d748d
yedidworembai <paitworem@gmail.com>
2026-02-26 02:20:42 +0700

    Modifikasi percobaan 3

--- Jobsheet2/Mahasiswa28.java
+++ Jobsheet2/Mahasiswa28.java
@@ -1,47 +1,48 @@
-
 public class Mahasiswa28 {
-
-    
     String nama;
     String nim;
     String kelas;
     double ipk;
 
-    
-    void tampilkanInformasi() {
-        System.out.println("Nama  : " + nama);
-        System.out.println("NIM   : " + nim);
-        System.out.println("Kelas : " + kelas);
-        System.out.println("IPK   : " + ipk);
+    public Mahasiswa28() {
     }
 
    
+    public Mahasiswa28(String nm, String ni, double i, String kls) {
+        nama = nm;
+        nim = ni;
+        ipk = i;
+        kelas = kls;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
 
-void updateIPK(double ipkBaru) {
-    if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-        ipk = ipkBaru;
-    } else {
-        System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+    void updateIPK(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
-}
-
 
     String nilaiKinerja() {
         if (ipk >= 3.5) {
             return "Kinerja Sangat Baik";
-        } 
-        else if (ipk >= 3.0) {
+        } else if (ipk >= 3.0) {
             return "Kinerja Baik";
-        }
-        else if (ipk >= 2.0) {
+        } else if (ipk >= 2.0) {
             return "Kinerja Cukup";
-        }
-        else {
+        } else {
             return "Kinerja Kurang";
         }
     }
-
 }
--- Jobsheet2/MahasiswaMain28.java
+++ Jobsheet2/MahasiswaMain28.java
@@ -1,18 +1,23 @@
 public class MahasiswaMain28 {
     public static void main(String[] args) {
-        // Proses Instansiasi
-        Mahasiswa28 mhs1 = new Mahasiswa28();
         
-        // Mengakses dan mengisi atribut
+        Mahasiswa28 mhs1 = new Mahasiswa28();
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
 
-        // Memanggil method
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
+       
+        Mahasiswa28 mhs2 = new Mahasiswa28("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+
+        Mahasiswa28 mhsYedid = new Mahasiswa28("Yedid Worembai", "2341720000", 3.8, "1G");
+        mhsYedid.tampilkanInformasi();
     }
 }
